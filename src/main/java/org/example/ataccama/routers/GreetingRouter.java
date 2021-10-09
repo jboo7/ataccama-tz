293cf2e1358adefa00719308f4f14612f19fcc4b
Maksym Tkachov <servelat20kg@gmail.com>
2021-10-09 13:25:20 +0200

    web mvc initial commit

---
+++ src/main/java/org/example/ataccama/routers/GreetingRouter.java
@@ -0,0 +1,13 @@
+package org.example.ataccama.routers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingRouter {
+    @GetMapping("/greeting")
+    public void greeting(Model model) {
+        model.addAttribute("name", "ABCD");
+    }
+}
